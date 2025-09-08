47eb225ed8fdce086d370a548aff81c6edc46330
Vudattu Praneethnadh <142204604+vpraneethnadh@users.noreply.github.com>
2025-09-08 12:28:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,25 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import wasm from 'vite-plugin-wasm'
-// import topLevelAwait from 'vite-plugin-top-level-await'
-
-// export default defineConfig({
-//   plugins: [react(),wasm(),topLevelAwait()],
-//   server: {
-//     port: 5173,
-//     historyApiFallback: true,
-//     proxy: {
-//       '/generate': {
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//         rewrite: (path) => path,
-//       },
-//     },
-//     headers: {
-//       "Cross-Origin-Opener-Policy": "same-origin",
-//       "Cross-Origin-Embedder-Policy": "require-corp"
-//     }
-//   },
-//   optimizeDeps: {
-//     exclude: ["sql.js/react"],
-//     esbuildOptions: {
-//       target: "es2020"
-//     }
-//   },
-//   build: {
-//     target: 'esnext'
-//   }
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
-  base: './',   // 👈 important
+  plugins: [react(),wasm(),topLevelAwait()],
+  server: {
+    port: 5173,
+    historyApiFallback: true,
+    proxy: {
+      '/generate': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    }
+  },
   optimizeDeps: {
     exclude: ["sql.js/react"],
     esbuildOptions: {
@@ -50,3 +30,4 @@ export default defineConfig({
     target: 'esnext'
   }
 });
+
